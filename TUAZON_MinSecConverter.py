e4f6ba22e34bd745d7110f80f7597addbe1fc217
nicoleshanetuazon2002 <144906962+nicoleshanetuazon2002@users.noreply.github.com>
2023-09-27 06:04:27 -0700

    Add files via upload

---
+++ TUAZON_MinSecConverter.py
@@ -0,0 +1,6 @@
+
+integer_seconds = int(input("Enter an Integer in seconds : "))
+minutes = integer_seconds // 60
+seconds = minutes % 
+print()
+print(integer_seconds, "seconds is", int(minutes), "minutes and" , int(seconds), "seconds.")
