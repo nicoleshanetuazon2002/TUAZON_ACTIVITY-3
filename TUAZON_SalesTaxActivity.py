3cc1db1eeb6f84e4c31572a678e7910f9a132754
nicoleshanetuazon2002 <144906962+nicoleshanetuazon2002@users.noreply.github.com>
2023-09-27 04:35:45 -0700

    Add files via upload

---
+++ TUAZON_SalesTaxActivity.py
@@ -0,0 +1,13 @@
+print("Name : Nicole Shane P. Tuazon")
+print("BSCS-1E")
+print()
+print("Sales Tax")
+print()
+price = eval(input("Enter purchase amount: "))
+print()
+tax = price * 0.06
+print("Your Sales Tax is:" , round(tax, 2))
+print()
+print("THANK YOU!")
+
+
